5a12db4821950bccc65a0a2c7a4578cc8514b0b7
Igor Ovchinnikov <faceplaces@inbox.ru>
2023-09-14 10:52:46 +0300

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const { default: koaBody } = require('koa-body');
 const Koa = require('koa');
 const Router = require('koa-router');
+const cors = require('koa2-cors');
 
 const app = new Koa();
 
